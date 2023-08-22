c3ea5518e240becbea2a123e00955083424a8f32
Arik Alexandrov <arikxl@gmail.com>
2023-08-22 22:17:34 +0300

    57 finish 11

--- js/5-7.js
+++ js/5-7.js
@@ -1,10 +1,122 @@
-const fiveSeven = (num) => {
-    if (num !== 5 && num !== 7) {
-        console.log('Please enter 5 or 7');
-    } else {   
-        const res = num === 7 ? 5 : 7;
-        console.log(`Got ${num}, Return ${res}`)
-    }
-}
+// IF ELSE
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log('Please enter 5 or 7');
+//     } else {
+//         const res = num === 7 ? 5 : 7;
+//         console.log(`Got ${num}, Return ${res}`)
+//     }
+// }
 
+// SUB
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//             const res = 12-num;
+//             console.log(`Got ${num}, Return ${res}`)
+//     }
+// }
+
+// DIV
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//             const res = 35/num;
+//             console.log(`Got ${num}, Return ${res}`)
+//     }
+// }
+
+// XOR
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//             console.log(`Got ${num}, Return ${(num ^= 2)}`)
+//     }
+// }
+
+// ARRAY
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//             const arr = [0,1,2,3,4,7,6,5]
+//             console.log(`Got ${num}, Return ${arr[num]}`)
+//     }
+// }
+
+
+// SWITCH CASE
+// const fiveSeven = (num) => {
+//         switch (num) {
+//             case 5:
+//                 console.log(`Got ${num}, Return 7`);
+//                 break
+//             case 7:
+//                  console.log(`Got ${num}, Return 5`);
+//                  break
+//             default:
+//                console.log(`Got ${num}. Please enter 5 or 7`);
+//         }
+// }
+
+// STRING REPLACE PARSEINT
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//         console.log(`Got ${num}, Return ${parseInt("57".replace(num, ""))}`)
+//     }
+// }
+
+// STRING REPLACE NUMBER
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//         console.log(`Got ${num}, Return ${+("57".replace(num, ""))}`)
+//     }
+// }
+
+// OBJECT
+// const fiveSeven = (num) => {
+//         const obj = {
+//             5: 7,
+//             7:5
+//         };
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//         console.log(`Got ${num}, Return ${obj[num]}`)
+//     }
+// }
+
+// DICTIONARY
+// const fiveSeven = (num) => {
+//     const dict = {};
+//     dict[5] = 7;
+//     dict[7] = 5;
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//         console.log(`Got ${num}, Return ${dict[num]}`)
+//     }
+// }
+
+// MATH
+// const fiveSeven = (num) => {
+//     if (num !== 5 && num !== 7) {
+//         console.log(`Got ${num}. Please enter 5 or 7`);
+//     } else {
+//         console.log(`Got ${num}, Return ${Math.ceil((95 - (num * num)) / 10)}`)
+//     }
+// }
+
+
+
+fiveSeven(4)
 fiveSeven(5)
+fiveSeven(6)
+fiveSeven(7)
